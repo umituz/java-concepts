78d7e889e8428de27ed6b452dbfa058fea2e091d
Ümit UZ <umituz998@gmail.com>
2020-04-11 19:02:17 +0300

    switch

---
+++ Switch/src/Main.java
@@ -0,0 +1,16 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        char grade = 'A';
+
+        switch (grade){
+            case 'A':
+                System.out.println("Awesome");
+                break;
+            default:
+                System.out.println("Not Awesome");
+                break;
+        }
+    }
+}
