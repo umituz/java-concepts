be579ad7efe35a1ef97c2c266e1be97cb845a82e
Ümit UZ <umituz998@gmail.com>
2020-04-11 18:45:28 +0300

    if conditions

---
+++ Conditions/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        int number = 20;
+        if (number < 20) {
+            System.out.println("Number is smaller than 20");
+        } else if (number == 20) {
+            System.out.println("Number is equal to 20");
+        } else {
+            System.out.println("Number is not smaller than 20");
+        }
+    }
+}
