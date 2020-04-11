52ce4a8cb889881f8e8fc50078d5007fa2eed8d2
Ümit UZ <umituz998@gmail.com>
2020-04-11 17:08:31 +0300

    DataTypes

---
+++ DataTypes/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean type = true;
+        char test = 'A';
+        byte number1 = 1;
+        short number2 = 11111;
+        int number3 = 599999999;
+        long number4 = 999999999;
+        float number6 = 4850;
+        double number5 = 45.90;
+    }
+}
