8c2bdd8881738d5415da115f79b73df870762598
Ümit UZ <umituz998@gmail.com>
2020-04-11 21:08:48 +0300

    string 2

--- String/src/Main.java
+++ String/src/Main.java
@@ -2,22 +2,33 @@ public class Main
 {
     public static void main(String[] args)
     {
-        String message = "MY NAME IS UMIT UZ";
+        String message = "My name is Ümit UZ";
 
-        System.out.println(message);
-        System.out.println("-------------");
-        System.out.println("Element count : " + message.length());
-        System.out.println("5. element : " + message.charAt(4));
-        System.out.println(message.concat(" AND I AM AWESOME"));
-        System.out.println(message.startsWith("M"));
-        System.out.println(message.endsWith("Z"));
-        System.out.println("-------------");
-        char[] characters = new char[7];
-        message.getChars(0,7,characters,0);
-        System.out.println(characters);
-        System.out.println("-------------");
-        System.out.println(message.indexOf('Y'));
-        System.out.println(message.lastIndexOf('U'));
+//        System.out.println(message);
+//        System.out.println("-------------");
+//        System.out.println("Element count : " + message.length());
+//        System.out.println("5. element : " + message.charAt(4));
+//        System.out.println(message.concat(" AND I AM AWESOME"));
+//        System.out.println(message.startsWith("M"));
+//        System.out.println(message.endsWith("Z"));
+//        System.out.println("-------------");
+//        char[] characters = new char[7];
+//        message.getChars(0,7,characters,0);
+//        System.out.println(characters);
+//        System.out.println("-------------");
+//        System.out.println(message.indexOf('Y'));
+//        System.out.println(message.lastIndexOf('U'));
+//
+//        System.out.println(message.replace(' ','-'));
+//        System.out.println(message.substring(3));
+//        System.out.println(message.substring(3,7));
+//
+//        for(String word:message.split(" ")){
+//            System.out.println(word);
+//        }
 
+        System.out.println(message.toLowerCase());
+        System.out.println(message.toUpperCase());
+        System.out.println(message.trim());
     }
 }
