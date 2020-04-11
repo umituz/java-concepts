145b87811da000cf3466b1376e12d5353b43b89e
Ümit UZ <umituz998@gmail.com>
2020-04-11 16:03:52 +0300

    Variables

--- HelloHome/src/Main.java
+++ HelloHome/src/Main.java
@@ -2,7 +2,7 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        System.out.println("Hello Home");
         System.out.println("My name is Ümit UZ");
     }
 }
---
+++ Variables/src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // java is a case sensitive language
+        int number = 10;
+        String message = "The number is : ";
+        System.out.println(message + number);
+        System.out.println(message + number);
+        System.out.println(message + number);
+    }
+}
