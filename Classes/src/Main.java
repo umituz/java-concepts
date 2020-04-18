0a7bdaa5b8154a28940be821f5517da1e8c46505
Ümit UZ <umituz998@gmail.com>
2020-04-19 01:30:09 +0300

    product and product manager

--- Classes/src/Main.java
+++ Classes/src/Main.java
@@ -1,21 +1,39 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
         referenceType();
         customer();
         fourOperations();
+        product();
     }
 
-    public static void fourOperations(){
+    public static void product() {
+        Product product = new Product();
+
+        product.id = 1;
+        product.name = "Computer";
+        product.description = "Beautiful Computer";
+        product.price = 99.99;
+        product.stockAmount = 0;
+
+        System.out.println(product.id);
+        System.out.println(product.name);
+        System.out.println(product.description);
+        System.out.println(product.price);
+        System.out.println(product.stockAmount);
+
+        ProductManager productManager = new ProductManager();
+        productManager.add(product);
+    }
+
+    public static void fourOperations() {
         FourOperations fourOperations = new FourOperations();
 
-        int result = fourOperations.plus(1,1);
+        int result = fourOperations.plus(1, 1);
 
         System.out.println(result);
     }
 
-    public static void customer(){
+    public static void customer() {
         Customer customer = new Customer();
         customer.create();
         customer.store();
@@ -24,7 +42,7 @@ public class Main
         customer.destroy();
     }
 
-    public static void referenceType(){
+    public static void referenceType() {
 
         int number1 = 10;
         int number2 = 20;
@@ -32,8 +50,8 @@ public class Main
         number2 = number1;
         System.out.println(number2);
 
-        int[] numbers1 = new int[]{1,2,3,4,5};
-        int[] numbers2 = new int[]{6,7,8,9,10};
+        int[] numbers1 = new int[]{1, 2, 3, 4, 5};
+        int[] numbers2 = new int[]{6, 7, 8, 9, 10};
 
         numbers2 = numbers1;
         numbers1[0] = 10;
