c986aaefac02eff97656284ad85bf7562549e6be
Ümit UZ <umituz998@gmail.com>
2020-04-23 00:48:05 +0300

    try catch finally and multiple catch

--- ExceptionHandling/src/Main.java
+++ ExceptionHandling/src/Main.java
@@ -6,8 +6,12 @@ public class Main
             int[] numbers = new int[]{1,2,3};
 
             System.out.println(numbers[5]);
+        }catch (StringIndexOutOfBoundsException exception){
+            System.out.println("StringIndexOutOfBoundsException : " + exception);
+        } catch (ArrayIndexOutOfBoundsException exception){
+            System.out.println("ArrayIndexOutOfBoundsException : " + exception);
         }catch (Exception exception){
-            System.out.println("An Unexpected Error : " + exception);
+            System.out.println("Exception Log : " + exception);
         }finally {
             System.out.println("Done");
         }
