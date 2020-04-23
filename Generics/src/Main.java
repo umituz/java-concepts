fc604b07b038c6ef59983262b6377b9cc6750f06
Ümit UZ <umituz998@gmail.com>
2020-04-23 18:34:56 +0300

    generics

---
+++ Generics/src/Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        MyList<Customer> myList = new MyList<Customer>();
+
+        myList.add(new Customer());
+        myList.add(new Customer());
+    }
+}
