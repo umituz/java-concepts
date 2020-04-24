99d1850fc93952c0061902114c7cb0dd95157f7d
Ümit UZ <umituz998@gmail.com>
2020-04-24 22:48:38 +0300

    update

--- JDBC/src/Main.java
+++ JDBC/src/Main.java
@@ -5,8 +5,9 @@ public class Main
 {
     public static void main(String[] args) throws SQLException
     {
-        create();
+//        create();
 //        read();
+        update();
     }
 
     public static void read() throws SQLException
@@ -34,6 +35,7 @@ public class Main
             connection.close();
         }
     }
+
     public static void create() throws SQLException
     {
         Connection connection = null;
@@ -47,6 +49,27 @@ public class Main
             statement.setString(2,"TUR");
             statement.setString(3,"Muha");
             statement.setInt(4,5000);
+            int result = statement.executeUpdate();
+            System.out.println("Result : " + result);
+
+        } catch (SQLException exception) {
+            databaseHelper.showErrorMessage(exception);
+        } finally {
+            statement.close();
+            connection.close();
+        }
+    }
+
+    public static void update() throws SQLException
+    {
+        Connection connection = null;
+        DatabaseHelper databaseHelper = new DatabaseHelper();
+        PreparedStatement statement = null;
+        try {
+            connection = databaseHelper.getConnection();
+            String sqlQuery = "update city set population = 100000, district = 'Turkey' where id = ?";
+            statement = connection.prepareStatement(sqlQuery);
+            statement.setInt(1,4081);
             int result = statement.executeUpdate();
             System.out.println("Result : " + result);
 
