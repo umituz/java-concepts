4c0093b97247282da6be55b6dc8d6e6e0be22c4f
Ümit UZ <umituz998@gmail.com>
2020-05-02 22:19:44 +0300

    string injection

--- spring/springIntro/src/springIntro/MsSqlCustomerDal.java
+++ spring/springIntro/src/springIntro/MsSqlCustomerDal.java
@@ -2,8 +2,21 @@ package springIntro;
 
 public class MsSqlCustomerDal implements ICustomerDal
 {
+	String connectionString;
+	
+	public String getConnectionString() 
+	{
+		return connectionString;
+	}
+
+	public void setConnectionString(String connectionString) 
+	{
+		this.connectionString = connectionString;
+	}
+	
 	public void add() 
 	{
+		System.out.println("Connection string : " + this.connectionString);
 		System.out.println("Added to MsSql Database");	
 	}
 }
--- spring/springIntro/src/springIntro/MySqlCustomerDal.java
+++ spring/springIntro/src/springIntro/MySqlCustomerDal.java
@@ -2,8 +2,21 @@ package springIntro;
 
 public class MySqlCustomerDal implements ICustomerDal 
 {
+	String connectionString;
+	
+	public String getConnectionString() 
+	{
+		return connectionString;
+	}
+
+	public void setConnectionString(String connectionString) 
+	{
+		this.connectionString = connectionString;
+	}
+	
 	public void add()
 	{
+		System.out.println("Connection string : " + this.connectionString);
 		System.out.println("Added to MySql Database");
 	}
 
